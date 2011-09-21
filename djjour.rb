be44649d94fbfcffdc8b423bd53e3feb09994305
Glen Maddern <glenmaddern@gmail.com>
2011-09-21 16:32:40 +1000

    added a dead simple sinatra server

--- djjour.rb
+++ djjour.rb
@@ -2,11 +2,11 @@
 
 require 'nokogiri'
 
-n = Nokogiri::XML(File.read("/Volumes/Rodríguez/Music/iTunes/iTunes Music Library.xml"))
+n = Nokogiri::XML(File.read(File.expand_path("~/Music/iTunes/iTunes Music Library.xml")))
 
 tracks_dict = n.xpath("*/dict/key[contains(text(), 'Tracks')]").first.next_element
 
-tracks_hash = tracks_dict.xpath("dict").map { |track|
+tracks = tracks_dict.xpath("dict").map { |track|
   h = {}
   track.elements.each_slice(2) do |key, val|
     raise "WATKEY #{key}" if key.name != 'key'
@@ -28,4 +28,24 @@ tracks_hash = tracks_dict.xpath("dict").map { |track|
   h
 }
 
-p tracks_hash.count
+p tracks.map { |t| t['Kind'] }.uniq
+
+require 'sinatra'
+require 'json'
+
+get '/' do
+  content_type :json
+  {count: tracks.count, tracks: tracks}.to_json
+end
+
+get '/mp3' do
+  content_type :json
+  mp3 = tracks.select { |t| t['Kind'] == "MPEG audio file" }
+  {count: mp3.count, tracks: mp3}.to_json
+end
+
+get '/aac' do
+  content_type :json
+  aac = tracks.select { |t| t['Kind'] =~ /AAC audio file/ }
+  {count: aac.count, tracks: aac}.to_json
+end
